0c5c1e643496d752e58d3cb1b5cdd020a70e84ea
Cheyanne Hewitt <hellkirien@gmail.com>
2025-08-03 09:18:10 -0500

    create handlers for building a successful call to query endpoint

--- dependency-mapper/NotionInterface.py
+++ dependency-mapper/NotionInterface.py
@@ -1,15 +1,28 @@
+import requests
 from requests import Response
+from dotenv import load_dotenv
+import os
 
 
 class NotionInterface:
     def __init__(self):
-        self.integration_key = 
         self.DATABASE_ID = "239725ab821d802ebefdf07739df73f5"
         self.BASE_URL = "https://api.notion.com/v1/"
-        self.endpoint = "/databases/" + self.DATABASE_ID + "/query"
-        self.headers = {
-            "Authorization": self.integration_key,
-            "Notion-Version": "2022-02-22",
-            "Content-Type": "application/json"
-        }
-        return self.headers
+        self.endpoint = "databases/" + self.DATABASE_ID + "/query"
+
+
+def get_private_key() -> str:
+    load_dotenv()
+    integration_key = os.getenv("NOTION_PRIVATE_TOKEN")
+    if not integration_key:
+        raise ValueError("NOTION_PRIVATE_TOKEN environment variable not set in .env.")
+    return integration_key
+
+
+def url() -> str:
+    return NotionInterface().BASE_URL + NotionInterface().endpoint
+
+
+def post_query(self, url: str, header: dict) -> Response:
+    response = requests.request("POST", url, headers=header)
+    return response
--- dependency-mapper/run.py
+++ dependency-mapper/run.py
@@ -1,1 +1,15 @@
-from NotionInterface import NotionInterface
+import NotionInterface
+import requests
+from requests import Response
+
+
+url = NotionInterface.url()
+header = {
+    "Authorization": NotionInterface.get_private_key(),
+    "Notion-Version": "2022-06-28",
+    "Content-Type": "application/json",
+}
+
+response = requests.post(url, headers=header)
+
+print(response.json())
